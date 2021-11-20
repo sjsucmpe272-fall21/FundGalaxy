d874cb48d71361f66808cef904635f401f01c482
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2021-11-20 15:28:00 -0800

    login and register

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,6 +22,8 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 // core components
 import Admin from "layouts/Admin.js";
 import RTL from "layouts/RTL.js";
+import Login from "layouts/Login.js";
+import Register from "layouts/Register.js";
 
 import "assets/css/material-dashboard-react.css?v=1.10.0";
 
@@ -30,6 +32,8 @@ ReactDOM.render(
     <Switch>
       <Route path="/admin" component={Admin} />
       <Route path="/rtl" component={RTL} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
   </BrowserRouter>,
