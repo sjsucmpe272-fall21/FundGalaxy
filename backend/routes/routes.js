0b377faf7b50e602010a57ad8f8be6516461f8d2
chelseyliu <msraxjlj@outlook.com>
2021-11-25 20:01:42 -0500

    support [AddCompany, InvestCompany, ListInvestedCompanies] API

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -79,7 +79,7 @@ router.put("/updateCompanyDetails/:companyid", async (req, res, next) => {
 
 // Update Investor Details
 router.put("/updateInvestorDetails/:investorid", async (req, res, next) => {
-  Companies.findOneAndUpdate(
+  Investors.findOneAndUpdate(
     { _id: req.params.investorid },
     {
       companiesInvestedIn: req.body.companiesInvestedIn,
@@ -141,5 +141,133 @@ router.get("/investors/all", async (req, res, next) => {
     }
   });
 });
+
+
+// Update Company Details
+router.post("/addCompanyDetails", async (req, res, next) => {
+  console.log(`Add new company ${req.body.name}`);
+  Companies.create(
+    {
+      domain: req.body.domain,
+      status: req.body.status,
+      categoryList: req.body.categoryList,
+      contact: req.body.contact,
+      description: req.body.description,
+      employeeCount: req.body.employeeCount,
+      foundedOn: req.body.foundedOn,
+      founders: req.body.founders,
+      homepageUrl: req.body.homepageUrl,
+      links: req.body.links,
+      name: req.body.name,
+      numFundingRounds: req.body.numFundingRounds,
+      people: req.body.people,
+      revenueRange: req.body.revenueRange,
+      totalFundingUsd: req.body.totalFundingUsd,
+    },
+    // { new: true },
+    (err, doc) => {
+      if (!err) {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify(doc));
+      } else {
+        res.writeHead(500, {
+          "Content-Type": "text/plain",
+        });
+        res.end("Error Occured");
+      }
+    }
+  );
+});
+
+
+// Add Investor Details
+router.post("/addInvestorDetails", async (req, res, next) => {
+  console.log(`Add investor ${req.body.name}`);
+  Investors.create(
+    {
+      companiesInvestedIn: req.body.companiesInvestedIn,
+      contact: req.body.contact,
+      description: req.body.description,
+      investmentDomains: req.body.investmentDomains,
+      investorType: req.body.investorType,
+      links: req.body.links,
+      name: req.body.name,
+      totalFunding: req.body.totalFunding,
+    },
+    // { new: true },
+    (err, doc) => {
+      if (!err) {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify(doc));
+      } else {
+        res.writeHead(500, {
+          "Content-Type": "text/plain",
+        });
+        res.end("Error Occured");
+      }
+    }
+  );
+});
+
+// Invest a company
+router.put("/investCompany/:investorid", async (req, res, next) => {
+  Companies.findById(
+    { _id: req.body.companiesInvestedIn.companyid },
+    (err, company) => {
+      if (!err) {
+        Investors.findOneAndUpdate(
+          { _id: req.params.investorid },
+          { $push: { companiesInvestedIn: req.body.companiesInvestedIn } },
+          { new: true },
+          (err, doc) => {
+            if (!err) {
+              res.writeHead(200, {
+                "Content-Type": "application/json",
+              });
+              res.end(JSON.stringify(doc));
+            } else {
+              res.writeHead(404, {
+                "Content-Type": "text/plain",
+              });
+              res.end("Investor not found.");
+            }
+          }
+        )
+      } else {
+        res.writeHead(404, {
+          "Content-Type": "text/plain",
+        });
+        res.end("Company not found.");
+      }
+    });
+});
+
+router.get("/investor/invested_companies/:investorid", async (req, res, next) => {
+  console.log(`Get invested companies of ${req.params.investorid}`)
+  Investors.findOne(
+    { _id: req.params.investorid }, 
+    (error, investor) => {
+    if (error) {
+      res.writeHead(404, {
+        "Content-Type": "text/plain",
+      });
+      res.end("Investor not found.");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      console.log(`response:\n ${investor.companiesInvestedIn}`);
+      res.end(JSON.stringify(investor.companiesInvestedIn));
+    }
+  });
+});
+
+
+
+
 
 module.exports = router;
